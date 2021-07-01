18ab05707f746d0c65095bf4f6291b558ebd9ab3
justformal <66424432+justformal@users.noreply.github.com>
2021-07-01 20:16:07 +0500

    Update file.js
    Обновил логику (update 1.1)

--- file.js
+++ file.js
@@ -4,9 +4,18 @@ let F1 = document.querySelector('.F1');
 let S2 = document.querySelector('.S2');
 let F2 = document.querySelector('.F2');
 let T2 = document.querySelector('.T2');
-
 let button = document.querySelector('.button');
-button.onclick = function() 
-{
-T2.textContent = +T1.value * ((+S2.value / +S1.value)*((+F2.value / +F1.value)**2)); 
-};
+let buttonmin = document.querySelector('.buttonmin');
+let buttonsec = document.querySelector('.buttonsec');
+
+
+function One() {T2.value = (+T1.value * ((+S2.value / +S1.value)*((+F2.value / +F1.value)**2))).toFixed(0); buttonmin.hidden=false; buttonsec.hidden=true;};
+function Two() {T2.value = (T2.value/60).toFixed(2); buttonmin.hidden=true; buttonsec.hidden=false};
+function Three() {T2.value = (T2.value*60).toFixed(0); buttonmin.hidden=false; buttonsec.hidden=true};
+
+
+button.onclick = One;   
+
+buttonmin.onclick = Two;
+
+buttonsec.onclick = Three;
